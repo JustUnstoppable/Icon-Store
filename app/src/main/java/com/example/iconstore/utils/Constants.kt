8d0c2f3ed9fa2d613ac96dc20a381a8c2bf9f9b9
Smriti123Shukla <200104057@hbtu.ac.in>
2022-05-13 22:41:34 +0530

    Icon Store

---
+++ app/src/main/java/com/example/iconstore/utils/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.iconstore.utils
+
+import retrofit2.http.Headers
+
+
+const val BASE_URL = "https://api.iconfinder.com/v4/"
+const val ICONS_URL = "icons/search"
+
+const val QUERY = "arrow"
+const val COUNT = "10"
+const val START_INDEX = "offset"
+const val CLIENT_ID = ""//please insert client id
+const val API_KEY = ""//please insert api id
+
+const val NUMBER_OF_ICONS = 20
